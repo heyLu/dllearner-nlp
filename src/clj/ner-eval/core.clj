7c7eb92dc0cb7950b8483526af29fddedcefaef4
Lucas Stadler <lu@papill0n.org>
2014-06-24 15:06:11 +0200

    add a rough plan.

--- src/clj/ner-eval/core.clj
+++ src/clj/ner-eval/core.clj
@@ -1,4 +1,34 @@
-(ns ner-eval
+(ns ner-eval.core
+  "Evaluate NER capabilities of nlp tools.
+
+# steps
+
+- retrieve entities from dbpedia (select by class)
+    * using the sparql endpoint
+    * which format to use? (json, csv, ...)
+- get abstracts for those entities
+- process abstracts using nlp tools
+    * start with nerd, it supports many things (api might be cumbersome, though)
+    * result are extracted entities (vector of places in the text with info)
+- basic stats
+    * number of extracted entities
+- more reasonable stats
+    * overlap
+        - which tools also find this
+        - do tools different tools find 'bigger' entities for the same (or similar) text?
+        - (maybe there's a library for diffing in clj?)
+    * which entities are mapped (different ones for the same text)
+- comparison with hand-tagged data
+    * how many places also matched
+    * to which entities
+
+# data model
+
+- everything in vectors
+- from sparql: just the names
+- with abstracts: {:name \"...\", :abstract \"...\"} (maybe also comment?)
+- entity extraction: vector of maps, name, abstract, entities:
+    * entities: `[{:text \"...\", :start 0, :end 10, :entity \"<url>\"}]`"
   (:require [clj-http.client :as http]))
 
 (defn dbpedia-query
