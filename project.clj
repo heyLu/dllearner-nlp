46ca246634988750c59f1459ee095f81931cabb7
Lucas Stadler <lu@papill0n.org>
2014-07-01 12:03:35 +0200

    fix classpath.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject ner-eval "0.0.1-alpha"
   :description "Evaluating NER capabilities of nlp tools."
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-http "0.9.2"]])
+                 [clj-http "0.9.2"]]
+  :source-paths ["src/clj"])
