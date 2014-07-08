104425020ec7d63d556318c98a891a5efb0aa28a
Lucas Stadler <lu@papill0n.org>
2014-07-08 12:42:04 +0200

    support overlaps by comparing to "reference" annotations.

--- src/clj/ner_eval/metrics.clj
+++ src/clj/ner_eval/metrics.clj
@@ -36,16 +36,47 @@
     (assoc {} :intersectionCount (count (:intersection c))
              :differenceCount (count (:difference c)))))
 
+(def overlap-example
+  "Peter Pan is a dear friend of the Great Doodadle of neverland who likes cake very much.")
+
+(def overlap-example-ranges
+  [[0 8]
+   [20 25]
+   [35 47] [39 47] [52 59] [35 59]])
+
 (defn overlap?
   "Overlap if boundaries of one range are within the other."
   [[s1 e1] [s2 e2]]
-  (or (< s1 s2 e1) (< s1 e2 e1)
-      (< s2 s1 e2) (< s2 e1 e2)))
+  (or (<= s1 s2 e1) (<= s1 e2 e1)
+      (<= s2 s1 e2) (<= s2 e1 e2)))
+
+(defn overlap [ann1 ann2]
+  (let [{s1 :start, e1 :end} ann1
+        {s2 :start, e2 :end} ann2]
+    (if (overlap? [s1 e1] [s2 e2])
+      {:text (:text ann1)
+       :ref (:text ann2)
+       :length (- (- e1 s1) (- e2 s2))}
+      nil)))
 
 (defn overlaps
   "Annotations whose ranges overlap"
-  [anns1 anns2]
-  ())
+  [anns ref-anns]
+  (filter (comp not nil?)
+    (map #(overlap %1 %2) anns ref-anns)))
+
+(defn result-overlaps [anns-for-texts refs-for-texts]
+  (let [texts (keys refs-for-texts)]
+    (seq
+     (flatten
+      (filter (comp not nil? seq)
+              (map (fn [text]
+                     (if (contains? anns-for-texts text)
+                       (let [anns (get anns-for-texts text)
+                             ref-anns (get refs-for-texts text)]
+                         (overlaps anns ref-anns))
+                       nil))
+                   texts))))))
 
 (defn collect-anns [text-anns]
   (into #{} (flatten (vals text-anns))))
@@ -83,13 +114,23 @@
                       {}
                       anns)))
 
-(defn annotation-stats [anns-map]
+(defn overlap-stats [{:keys [anns-for-texts ref-anns] :as m}]
+  (if ref-anns
+    (let [overlaps (result-overlaps anns-for-texts ref-anns)]
+      (assoc m
+        :overlaps (coll-stats (map :length overlaps))))
+    m))
+
+(defn annotation-stats [anns-map & [ref-anns]]
   (dissoc
    (let [m (assoc {}
+             :anns-for-texts anns-map
+             :ref-anns ref-anns
              :anns (flatten (vals anns-map))
              :texts (keys anns-map))]
      (->> m
           count-stats
           word-stats
-          mapping-stats))
-   :anns :texts))
+          mapping-stats
+          overlap-stats))
+   :anns :texts :anns-for-texts :ref-anns))
